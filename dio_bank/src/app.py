7e07d6b0c0e0be200505587dcdf976df52d73587
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-04-15 15:14:23 -0300

    Criando End-Point de users

--- dio_bank/src/app.py
+++ dio_bank/src/app.py
@@ -1,7 +1,71 @@
-from flask import Flask
+from datetime import datetime
+import os
 
-app = Flask(__name__)
+import click
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
 
-@app.route("/olamundo/", methods=["POST", "GET", "PUT", "DELETE"])
-def hello_world():
-    return "<p>Hello, World!</p>"
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]= mapped_column(nullable=False)
+    body: Mapped[str]= mapped_column(nullable=False)
+    created: Mapped[datetime] = mapped_column(server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, body={self.body!r}), created={self.created!r}, author_id={self.author_id!r})"
+
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
+
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI='sqlite:///diobank.sqlite',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.cli.add_command(init_db_command)
+    db.init_app(app)
+
+    from dio_bank.src.controller import user, post
+    app.register_blueprint(user.app)
+    return app
---
+++ dio_bank/src/controller/user.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, request
+from dio_bank.src.app import User, db
+
+app = Blueprint('user', __name__, url_prefix='/users')
+
+def _create_user():
+    data = request.json
+    user = User(username=data["username"])
+    db.session.add(user)
+    db.session.commit()
+
+
+def _list_users():
+    query = db.select(User)
+    users = db.session.execute(query).scalars()
+    return [
+        {
+            "id": user.id,
+            "username": user.username
+        }
+        for user in users
+    ]
+
+@app.route('/', methods=["GET", "POST"])
+def handle_user():
+    if request.method == "POST":
+        _create_user()
+        return {"message": "User Created!"}, 201
+    else:
+        return {"users": _list_users()}
